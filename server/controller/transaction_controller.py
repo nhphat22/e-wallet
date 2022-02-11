82e7755863fcc2c2a1b6e9c01214a1f9d2d74146
nhphat22 <trafaldwaterlaw@gmail.com>
2022-02-12 00:24:56 +0700

    update create transaction

--- server/controller/transaction_controller.py
+++ server/controller/transaction_controller.py
@@ -1,22 +1,40 @@
 from flask.views import MethodView
 from flask import request, make_response, jsonify
+from hashlib import md5
+import hmac
+import json
 
 from server.database import db
 from server.models.transaction_model import Transaction
+from server.models.merchant_model import Merchant
+
 
 class CreateTransactionAPI(MethodView):
     def post(self):
-        print(request.form)
+        data = request.form.to_dict()
+        merchant = Merchant.query.filter_by(merchantId=data["merchantId"]).first()
+        hash_key=str(merchant.apiKey)
+        signature = hmac.new(hash_key.encode('utf-8'), json.dumps({
+                'merchantId': data["merchantId"],
+                'amount': float(data["amount"]),
+                'extraData': data["extraData"]
+            }, sort_keys=True).encode('utf-8'), md5).hexdigest()
+        if data["signature"] != signature:
+            responseObject = {
+                    'status': 'fail',
+                    'message': 'Security alert!'
+                }
+            return make_response(jsonify(responseObject)), 400
         transaction = Transaction(
-            merchantId=request.form["merchantId"],
-            amount=request.form["amount"],
-            incomeAccount="9c7d101d-dcec-41b6-9627-8e15cd5bc6b8",
+            merchantId=data["merchantId"],
+            amount=data["amount"],
+            incomeAccount=merchant.accountId,
             outcomeAccount=None,
-            signature=request.form["signature"]
+            signature=data["signature"]
         )
-        # insert the merchant account
-        db.session.add(transaction)
-        db.session.commit()
+        # insert the transaction
+        # db.session.add(transaction)
+        # db.session.commit()
         responseObject = {
             'status': 'success',
             'data': {
@@ -26,4 +44,4 @@ class CreateTransactionAPI(MethodView):
                     'signature': transaction.signature
                 }
         }
-        return make_response(jsonify(responseObject)), 201
+        return make_response(jsonify(responseObject)), 201    
